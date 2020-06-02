a49664ae536c00fcae79ab72e0a1c0a0fddf7e9d
guillermo varela <guillovarela16@gmail.com>
2020-06-02 19:25:54 -0300

    Division model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_221825) do
+ActiveRecord::Schema.define(version: 2020_06_02_222534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "divisions", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
